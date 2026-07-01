12c3a1eea65f316054ca571114a6ca1fc2f65cda
cuelgue <cuelguerandom@gmail.com>
2026-07-01 17:12:10 -0300

    se agrego un prompt básico, y se separaron las funciones cargar_buffer y print_bs. ademas se agregaron las constantes T_BS y T_DEL

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 
 #define MAX_BUFFER  512
 #define MAX_TOK  64
+#define T_DEL  127
+#define T_BS  8
 
 typedef struct {
   char buffer[MAX_BUFFER];
@@ -33,20 +35,6 @@ int getch(void)
     return ch;
 }
 
-/* reads from keypress, echoes */
-//Este probablemente noi lo use, porque me imprime todas las teclas,
-int getche(void)
-{
-    struct termios oldattr, newattr;
-    int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
-    newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
-    return ch;
-}
 
 bool exit_shell()
 {
@@ -80,28 +68,53 @@ void separar_token(char *b, char **token, int *n )
 
 } 
 
+/* Cuando apreto la tecla back space
+*me la tima como la tecla de borrado.
+*esta funcion imprime la tecla bs, un espacio
+*y de nuevo la tecla bs. dando la senciacion de borrado
+*como en cualquier otra shell */
+void print_bs()
+{
+  printf("%c",T_BS);
+  printf(" ");
+  printf("%c",T_BS);
+}
+
+void cargar_buffer(input_t *input)
+{
+  input->c = getch();
+  if (input->cant < MAX_BUFFER - 1 && input->c != '\n' && input->c != T_DEL) {
+    input->buffer[input->cant] = input->c;       
+    input->cant++;
+    input->buffer[input->cant] = '\0';
+  }
+}
 
 int main()
 {
   bool vf = false;
   input_t input = {0}; 
-  (void)vf;
-    while (!vf) {
-      input.c = getche();
-      if (input.cant < MAX_BUFFER - 1 && input.c != '\n') {
-        input.buffer[input.cant] = input.c;       
-        input.cant++;
-        input.buffer[input.cant] = '\0';
-      } else if (input.c == '\n') {
-        char *token[MAX_TOK];
-        int cant = 0;
-        separar_token(input.buffer,token,&cant);
-        ejecutar_comando(token,cant,&vf);
-        input.cant = 0;
+  while (!vf) {
+    printf("chan > ");
+    input.c = 0;
+    while (!vf && input.c != '\n'){      
+      cargar_buffer(&input);
+        if (input.c == '\n') {
+          char *token[MAX_TOK];
+          int cant = 0;
+          separar_token(input.buffer,token,&cant);
+          ejecutar_comando(token,cant,&vf);
+          input.cant = 0;
+        }
+      if ((input.c == T_BS || input.c == T_DEL)) {
+        if (input.cant > 0) {
+          input.cant--;
+          print_bs();
+        }
       }
+      printf("%c",input.c);    
     }
 
-
- 
+  }
   return 0;
 }
